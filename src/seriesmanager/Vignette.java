0551f582f3b6279bca9420375309223d92ab643e
AM-MIAGE <arles.mathieu@gmail.com>
2014-05-16 17:03:10 +0200

    Création package database

--- src/seriesmanager/Vignette.java
+++ src/seriesmanager/Vignette.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
