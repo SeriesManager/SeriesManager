d3118f9d01ea3035b812bd8d2f4df5dbacfd503f
AM-MIAGE <arles.mathieu@gmail.com>
2014-05-16 17:21:50 +0200

    Remplissage des classes saison et serie

--- src/objets/Saison.java
+++ src/objets/Saison.java
@@ -6,10 +6,53 @@
 
 package objets;
 
+import java.util.Date;
+
 /**
  *
  * @author Arles Mathieu
  */
 public class Saison {
+    
+    private int id;
+    private Date date_sortie;
+    private String synopsis;
+    private int id_serie;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate_sortie() {
+        return date_sortie;
+    }
+
+    public void setDate_sortie(Date date_sortie) {
+        this.date_sortie = date_sortie;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public int getId_serie() {
+        return id_serie;
+    }
+
+    public void setId_serie(int id_serie) {
+        this.id_serie = id_serie;
+    }
+    
+    
+    
+    
     
 }
--- src/objets/Serie.java
+++ src/objets/Serie.java
@@ -12,4 +12,90 @@ package objets;
  */
 public class Serie {
     
+    private int id;
+    private String name;
+    private String realisateur;
+    private String acteurs;
+    private String pays;
+    private String genre;
+    private String Statut;
+    private boolean vue;
+    private String commentaire;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealisateur() {
+        return realisateur;
+    }
+
+    public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+    }
+
+    public String getActeurs() {
+        return acteurs;
+    }
+
+    public void setActeurs(String acteurs) {
+        this.acteurs = acteurs;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getStatut() {
+        return Statut;
+    }
+
+    public void setStatut(String Statut) {
+        this.Statut = Statut;
+    }
+
+    public boolean isVue() {
+        return vue;
+    }
+
+    public void setVue(boolean vue) {
+        this.vue = vue;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+    
+    
+    
+    
+    
 }
